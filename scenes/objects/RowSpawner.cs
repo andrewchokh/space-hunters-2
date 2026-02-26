088a75e3d791e9a158d6bbfd115f11b4211e6132
trrt54999 <moki22810sobakacom@gmail.com>
2026-02-26 15:05:14 +0200

    Add calculate entity position along the X axis

--- scenes/objects/RowSpawner.cs
+++ scenes/objects/RowSpawner.cs
@@ -5,6 +5,8 @@ public partial class RowSpawner : Node2D
 {
     [Export]
     public PackedScene Entity;
+    [Export]
+    public float OffsetX = 30.0f;
 
     public override void _Ready()
     {
@@ -18,7 +20,8 @@ public partial class RowSpawner : Node2D
         int randomRowIndex = GD.RandRange(0, rowCount - 1);
 
         var enemyInstance = Entity.Instantiate<CharacterBody2D>();
-        enemyInstance.GlobalPosition = new Vector2(GlobalPosition.X, MapManager.Instance.GetRowY(randomRowIndex));
+        enemyInstance.GlobalPosition = new Vector2(GetViewportRect().Size.X + OffsetX,
+            MapManager.Instance.GetRowY(randomRowIndex));
 
         GetParent().AddChild(enemyInstance);
     }
