af33c65bdbe0720a86ad0869cc0515082e42ee89
trrt54999 <moki22810sobakacom@gmail.com>
2026-02-26 21:19:45 +0200

    feat: add a method to subtracts Damage from Health.

--- scenes/components/HealthComponent.cs
+++ scenes/components/HealthComponent.cs
@@ -7,4 +7,6 @@ public partial class HealthComponent : Node2D
     public int Health = 6;
     [Export]
     public int Protection = 1;
+
+    public void TakeDamage(int damage) => Health -= Mathf.Max(1, damage - Protection);
 }
