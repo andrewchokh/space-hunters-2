c4c37f7f6d705d706d5746d9eee13eca7e06be19
trrt54999 <moki22810sobakacom@gmail.com>
2026-02-26 21:32:23 +0200

    add a signal that tracks when health value is at 0 or less

--- scenes/components/HealthComponent.cs
+++ scenes/components/HealthComponent.cs
@@ -5,6 +5,11 @@ public partial class HealthComponent : Node2D
 {
     [Signal]
     public delegate void HealthChangedEventHandler(int oldHealth, int newHealth);
+    [Signal]
+    public delegate void EntityDiedEventHandler();
+
+    [Export]
+    public CharacterBody2D Entity;
 
     private int _health = 6;
     [Export]
@@ -17,10 +22,24 @@ public partial class HealthComponent : Node2D
             _health = Mathf.Max(0, value);
 
             EmitSignal(SignalName.HealthChanged, oldHealth, _health);
+
+            if (_health <= 0)
+                EmitSignal(SignalName.EntityDied);
         }
     }
     [Export]
     public int Protection = 1;
+
+    public override void _Ready()
+    {
+        if (Entity == null)
+        {
+            GD.PushError($"{Name}: Entity is not assigned!");
+            return;
+        }
+
+        EntityDied += Entity.QueueFree;
+    }
 
     public void TakeDamage(int damage) => Health -= Mathf.Max(1, damage - Protection);
 }
