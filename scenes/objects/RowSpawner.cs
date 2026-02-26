5d789c7b853e76f96a9907993520d891eb700085
trrt54999 <moki22810sobakacom@gmail.com>
2026-02-26 14:02:39 +0200

    feat: connect timer and generate row index

--- scenes/objects/RowSpawner.cs
+++ scenes/objects/RowSpawner.cs
@@ -3,15 +3,14 @@ using System;
 
 public partial class RowSpawner : Node2D
 {
-    [Export]
-    public PackedScene Entity;
-
     public override void _Ready()
     {
-        if (Entity == null)
-        {
-            GD.PushError($"{Name}: Entity is not assigned!");
-            return;
-        }
+        Timer timer = GetNode<Timer>("Timer");
+        timer.Timeout += SpawnEntity;
+    }
+
+    private void SpawnEntity()
+    {
+        int randomRowIndex = GD.RandRange(0, 4);
     }
 }
