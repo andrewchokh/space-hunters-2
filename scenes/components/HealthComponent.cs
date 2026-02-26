214a0a0f82c3e0c53f35039c0384b10ae8b0286d
trrt54999 <moki22810sobakacom@gmail.com>
2026-02-26 21:26:57 +0200

    feat: add a signal that tracks when health value is changed

--- scenes/components/HealthComponent.cs
+++ scenes/components/HealthComponent.cs
@@ -3,8 +3,22 @@ using System;
 
 public partial class HealthComponent : Node2D
 {
+    [Signal]
+    public delegate void HealthChangedEventHandler(int oldHealth, int newHealth);
+
+    private int _health = 6;
     [Export]
-    public int Health = 6;
+    public int Health
+    {
+        get => _health;
+        set
+        {
+            int oldHealth = _health;
+            _health = Mathf.Max(0, value);
+
+            EmitSignal(SignalName.HealthChanged, oldHealth, _health);
+        }
+    }
     [Export]
     public int Protection = 1;
 
