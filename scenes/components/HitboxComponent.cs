48f93227695d2cca7d969899b9bb7c770d4c892b
trrt54999 <moki22810sobakacom@gmail.com>
2026-03-01 12:36:54 +0200

    feat: add method to accepts damage value as an argument, an applies it to the Health System

--- scenes/components/HitboxComponent.cs
+++ scenes/components/HitboxComponent.cs
@@ -3,5 +3,17 @@ using System;
 
 public partial class HitboxComponent : Area2D
 {
-    
+    [Export]
+    public HealthComponent HealthComponent;
+
+    public override void _Ready()
+    {
+        if (HealthComponent == null)
+        {
+            GD.PushError($"{Name}: Component is not assigned!");
+            return;
+        }
+    }
+
+    public void Damage(int value) => HealthComponent?.TakeDamage(value);
 }
