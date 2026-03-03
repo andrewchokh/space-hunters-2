0bd8fce0d0e2b20327e8daa805b2ab59e678b7da
trrt54999 <moki22810sobakacom@gmail.com>
2026-03-03 23:19:36 +0200

    fix: change physics to physics process

--- scenes/objects/Bullet.cs
+++ scenes/objects/Bullet.cs
@@ -6,6 +6,6 @@ public partial class Bullet : Area2D
     [Export]
     public float Speed = 200.0f;
 
-    public override void _Process(double delta) =>
+    public override void _PhysicsProcess(double delta) =>
         GlobalPosition += new Vector2(Speed * (float) delta, 0);
 }
