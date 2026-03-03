7d7d070317baccf744d79e2289e4752d6a81a83d
trrt54999 <moki22810sobakacom@gmail.com>
2026-03-03 23:14:58 +0200

    feat: add bullet movement, add input map fire

--- scenes/objects/Bullet.cs
+++ scenes/objects/Bullet.cs
@@ -3,4 +3,9 @@ using System;
 
 public partial class Bullet : Area2D
 {
+    [Export]
+    public float Speed = 200.0f;
+
+    public override void _Process(double delta) =>
+        GlobalPosition += new Vector2(Speed * (float) delta, 0);
 }
