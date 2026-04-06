299c58f38e447640aed7588b4c68022c7bc41bb0
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-06 12:09:05 +0300

    feat: add summaries

--- scenes/components/BountyComponent.cs
+++ scenes/components/BountyComponent.cs
@@ -1,6 +1,9 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Grants a specified amount of score to the player when the attached entity is defeated.
+/// </summary>
 public partial class BountyComponent : Node2D
 {
    [Export]
@@ -10,11 +13,15 @@ public partial class BountyComponent : Node2D
 
    public override void _Ready()
    {
-       Setup();
+       if (!Setup()) return;
+
        HealthComponent.EntityDied += OnEntityDied;
    }
 
    private bool Setup() => this.IsAssigned(HealthComponent, nameof(HealthComponent));
 
+   /// <summary>
+   /// Handles the EntityDied event by increasing the global score.
+   /// </summary>
    private void OnEntityDied() => ScoreManager.Instance.Score += ScoreForKill;
 }
--- scripts/globals/ScoreManager.cs
+++ scripts/globals/ScoreManager.cs
@@ -1,6 +1,9 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Manages the global score for the player throughout the game session.
+/// </summary>
 public partial class ScoreManager : Node
 {
     public static ScoreManager Instance { get; private set; }
@@ -12,6 +15,15 @@ public partial class ScoreManager : Node
         Instance = this;
     }
 
+    /// <summary>
+    /// Gets or sets the player's current score.
+    /// </summary>
+    /// <value>The total score accumulated by the player.</value>
+    /// <remarks>
+    /// The score logic is strictly increasing. If a newly assigned value is less than
+    /// or equal to the current score, the assignment is ignored. This prevents accidental
+    /// score deductions during standard gameplay.
+    /// </remarks>
     public int Score
     {
         get => _score;
