3b6873ac653320afc471ff766b88ce648429e488
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-06 11:58:40 +0300

    feat: add value checker to score manager, clean code

--- scenes/components/BountyComponent.cs
+++ scenes/components/BountyComponent.cs
@@ -3,4 +3,18 @@ using System;
 
 public partial class BountyComponent : Node2D
 {
+   [Export]
+   public int ScoreForKill = 100;
+   [Export]
+   public HealthComponent HealthComponent;
+
+   public override void _Ready()
+   {
+       Setup();
+       HealthComponent.EntityDied += OnEntityDied;
+   }
+
+   private bool Setup() => this.IsAssigned(HealthComponent, nameof(HealthComponent));
+
+   private void OnEntityDied() => ScoreManager.Instance.Score += ScoreForKill;
 }
--- scripts/globals/ScoreManager.cs
+++ scripts/globals/ScoreManager.cs
@@ -3,4 +3,26 @@ using System;
 
 public partial class ScoreManager : Node
 {
+    public static ScoreManager Instance { get; private set; }
+
+    private int _score;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public int Score
+    {
+        get => _score;
+        set
+        {
+            if (value <= _score)
+            {
+                return;
+            }
+            _score = value;
+            this.DebugLog("New Score: " + Score);
+        }
+    }
 }
