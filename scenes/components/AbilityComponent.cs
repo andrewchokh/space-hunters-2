99c80579e999ed39de77b137474551a614d73d57
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-06 13:19:36 +0300

    feat: implement ability component, add component to player

--- scenes/components/AbilityComponent.cs
+++ scenes/components/AbilityComponent.cs
@@ -3,4 +3,45 @@ using System;
 
 public partial class AbilityComponent : Node2D
 {
+    [Export]
+    public double AbilityTime = 3.0;
+    [Export]
+    public double Cooldown = 10.0;
+
+    private Timer _timer;
+    private Timer _cooldownTimer;
+
+    public override void _Ready()
+    {
+        _timer = new Timer();
+        _timer.OneShot = true;
+        AddChild(_timer);
+
+        _cooldownTimer = new Timer();
+        _cooldownTimer.OneShot = true;
+        AddChild(_cooldownTimer);
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ability"))
+            Ability();
+    }
+
+    private async void Ability()
+    {
+        if (_cooldownTimer.TimeLeft > 0)
+        {
+            this.DebugLog($"Ability cooldown!!! ({Math.Round(_cooldownTimer.TimeLeft, 1)}s left)");
+            return;
+        }
+
+        this.DebugLog("Ability activated!");
+        _timer.Start(AbilityTime);
+
+        await ToSignal(_timer, Timer.SignalName.Timeout);
+
+        this.DebugLog("Ability timed out!");
+        _cooldownTimer.Start(Cooldown);
+    }
 }
