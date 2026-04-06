021ee264dd6774710fc5d4527f4a01adece35f9b
andrewchokh <andrew.chokh@example.com>
2026-04-06 14:53:02 +0300

    refactor: make AbilityPattern abstract and add methods

--- resources/ability_patterns/AbilityPattern.cs
+++ resources/ability_patterns/AbilityPattern.cs
@@ -5,10 +5,14 @@ using System;
 /// Defines the data and timing parameters for a specific ability.
 /// </summary>
 [GlobalClass]
-public partial class AbilityPattern : Resource
+public abstract partial class AbilityPattern : Resource
 {
     [Export]
     public double Duration = 3.0;
     [Export]
     public double Cooldown = 10.0;
+
+    public abstract void Enter(CharacterBody2D entity);
+    public abstract void Execute(CharacterBody2D entity);
+    public abstract void Exit(CharacterBody2D entity);
 }
