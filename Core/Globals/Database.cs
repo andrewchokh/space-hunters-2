e1040bb50def1da89d642a5b4682ab4a8d811586
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-26 14:30:54 +0300

    feat: add singleton pattern to database

--- Core/Globals/Database.cs
+++ Core/Globals/Database.cs
@@ -1,6 +1,13 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Database : Node
 {
+    public static Database Instance { get; private set; }
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
 }
