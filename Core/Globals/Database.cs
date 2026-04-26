3546ab1111aa12c5610394fe6e56da6b74ce324d
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-26 14:31:14 +0300

    feat: add basic fields to spaceship data

--- Core/Globals/Database.cs
+++ Core/Globals/Database.cs
@@ -6,6 +6,11 @@ public partial class Database : Node
 {
     public static Database Instance { get; private set; }
 
+    [Export]
+    public SpaceshipData[] Spaceships;
+
+    private Dictionary<string, SpaceshipData> SpaceshipData;
+
     public override void _Ready()
     {
         Instance = this;
