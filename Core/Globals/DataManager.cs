36e4f554cac4249c39da324f5122990af767b568
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-29 18:45:48 +0300

    feat: implement resources store

--- Core/Globals/DataManager.cs
+++ Core/Globals/DataManager.cs
@@ -2,44 +2,37 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// A global database singleton that stores and provides access to core game entity resources.
+/// Use this to retrieve resource files for spaceships and abilities from any point in the project.
+/// </summary>
+/// <remarks>
+/// PowerupData implementation is currently skipped as the type does not exist in the project yet.
+/// </remarks>
 public partial class DataManager : Node
 {
     [Export]
     public SpaceshipData[] Spaceships;
-    //  [Export]
-    //  public AbilityData[] Abilities;
+    [Export]
+    public AbilityData[] Abilities;
 
     public static DataManager Instance { get; private set; }
 
-    private Dictionary<string, SpaceshipData> _spaceshipData;
-    // private Dictionary<string, AbilityData> _abilityData;
+    public readonly Dictionary<string, SpaceshipData> SpaceshipData = new();
+    public readonly Dictionary<string, AbilityData> AbilityData = new();
 
+    /// <summary>
+    /// Initializes the singleton instance and populates the lookup dictionaries
+    /// from the exported resource arrays.
+    /// </summary>
     public override void _Ready()
     {
         Instance = this;
-        _spaceshipData = InitializeData(Spaceships);
-        //   _abilityData = InitializeData(Abilities);
-    }
 
-    public SpaceshipData GetSpaceship(string id) => _spaceshipData.GetValueOrDefault(id);
+        if(Spaceships != null)
+            foreach (var s in Spaceships) SpaceshipData[s.ID] = s;
 
-    // public  AbilityData GetAbility(string id) => _abilities.GetValueOrDefault(id);
-
-    private static Dictionary<string, T> InitializeData<T>(T[] rawData) where T : IIdentifiable
-    {
-        var dictionary = new Dictionary<string, T>();
-
-        if (rawData == null)
-            return dictionary;
-
-        foreach (var item in rawData)
-        {
-            if (item == null)
-                continue;
-
-            dictionary.TryAdd(item.ID, item);
-        }
-
-        return dictionary;
+        if (Abilities != null)
+            foreach (var s in Abilities) AbilityData[s.ID] = s;
     }
 }
