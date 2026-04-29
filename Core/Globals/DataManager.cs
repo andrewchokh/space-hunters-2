7c7c33fd792a03e79055e84cc9daa9e3d24ab1a9
trrt54999 <moki22810sobakacom@gmail.com>
2026-04-29 18:20:32 +0300

    feat: add global dictionary to iitialize data

--- Core/Globals/DataManager.cs
+++ Core/Globals/DataManager.cs
@@ -4,15 +4,42 @@ using System.Collections.Generic;
 
 public partial class DataManager : Node
 {
-    public static DataManager Instance { get; private set; }
-
     [Export]
     public SpaceshipData[] Spaceships;
+    //  [Export]
+    //  public AbilityData[] Abilities;
 
-    private Dictionary<string, SpaceshipData> SpaceshipData;
+    public static DataManager Instance { get; private set; }
+
+    private Dictionary<string, SpaceshipData> _spaceshipData;
+    // private Dictionary<string, AbilityData> _abilityData;
 
     public override void _Ready()
     {
         Instance = this;
+        _spaceshipData = InitializeData(Spaceships);
+        //   _abilityData = InitializeData(Abilities);
+    }
+
+    public SpaceshipData GetSpaceship(string id) => _spaceshipData.GetValueOrDefault(id);
+
+    // public  AbilityData GetAbility(string id) => _abilities.GetValueOrDefault(id);
+
+    private static Dictionary<string, T> InitializeData<T>(T[] rawData) where T : IIdentifiable
+    {
+        var dictionary = new Dictionary<string, T>();
+
+        if (rawData == null)
+            return dictionary;
+
+        foreach (var item in rawData)
+        {
+            if (item == null)
+                continue;
+
+            dictionary.TryAdd(item.ID, item);
+        }
+
+        return dictionary;
     }
 }
