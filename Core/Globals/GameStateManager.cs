124e4360ae4b1011bd2cc9a7ea50492d975dfd4a
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-10 00:09:24 +0200

    feat: add is input alloved method

--- Core/Globals/GameStateManager.cs
+++ Core/Globals/GameStateManager.cs
@@ -3,9 +3,22 @@ using System;
 
 public partial class GameStateManager : Node
 {
+	private GameState _currentState;
+	public GameState CurrentState => _currentState;
 	public static GameStateManager Instance { get; private set; }
 
-	public override void _Ready() => Instance = this;
+	public override void _Ready()
+	{
+		Instance = this;
+	}
+
+	public bool IsInputAllowed()
+	{
+		if (_currentState is GameState.Cutscene or GameState.GameOver)
+			return false;
+
+		return true;
+	}
 
 	public enum GameState
 	{
