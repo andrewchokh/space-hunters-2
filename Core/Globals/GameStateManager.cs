df5d0d72c49cc1ba02f0a2cf9653f89ca1cbdd37
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-10 00:07:54 +0200

    feat: add singleton to game state manager

--- Core/Globals/GameStateManager.cs
+++ Core/Globals/GameStateManager.cs
@@ -3,6 +3,10 @@ using System;
 
 public partial class GameStateManager : Node
 {
+	public static GameStateManager Instance { get; private set; }
+
+	public override void _Ready() => Instance = this;
+
 	public enum GameState
 	{
 		MainMenu,
