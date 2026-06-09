ebf44b108396114bd9a9dea7ab29b0a5ec06f0b2
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-10 00:10:37 +0200

    feat: add change state method with basic guard checks

--- Core/Globals/GameStateManager.cs
+++ Core/Globals/GameStateManager.cs
@@ -7,9 +7,33 @@ public partial class GameStateManager : Node
 	public GameState CurrentState => _currentState;
 	public static GameStateManager Instance { get; private set; }
 
-	public override void _Ready()
+	public override void _Ready() => Instance = this;
+	
+
+	public void ChangeState(GameState newState)
 	{
-		Instance = this;
+		if (_currentState == newState)
+			return;
+
+		if (newState == GameState.Paused && _currentState != GameState.Playing)
+			return;
+
+		_currentState = newState;
+
+		switch (newState)
+		{
+			case GameState.MainMenu:
+				break;
+			case GameState.Playing:
+				break;
+			case GameState.Paused:
+				GetTree().Paused = true;
+				break;
+			case GameState.GameOver:
+				break;
+			case GameState.Cutscene:
+				break;
+		}
 	}
 
 	public bool IsInputAllowed()
