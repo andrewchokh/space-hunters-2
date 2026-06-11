758393685ede33c5ef83f00fcfe7eab21f303652
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-11 19:35:03 +0200

    feat: create basic game over scene and script

---
+++ UI/Menus/GameOverMenu.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class GameOverMenu : Control
+{
+	[Export]
+	public Button TryAgainButton;
+	[Export]
+	public Button BackToMainMenuButton;
+	[Export]
+	public Button QuitTheGameButton;
+	[Export]
+	public Label ScoreLabel;
+
+
+}
