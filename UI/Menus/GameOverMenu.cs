c0accd04d2d84200b05f189f03b05a6a87e1f1e2
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-11 19:35:34 +0200

    feat: add basic methods

--- UI/Menus/GameOverMenu.cs
+++ UI/Menus/GameOverMenu.cs
@@ -12,5 +12,26 @@ public partial class GameOverMenu : Control
 	[Export]
 	public Label ScoreLabel;
 
+	public override void _Ready()
+	{
+		GameStateManager.Instance.ChangeState(GameState.GameOver);
+		ScoreLabel.Text = ScoreManager.Instance.Score.ToString(); // ???
+		TryAgainButton.Pressed += TryAgainButtonPressed;
+		BackToMainMenuButton.Pressed += BackToMainMenuButtonPressed;
+		QuitTheGameButton.Pressed += QuitTheGameButtonPressed;
+	}
 
+	private void TryAgainButtonPressed()
+	{
+		GameStateManager.Instance.ChangeState(GameState.Playing);
+		GetTree().ChangeSceneToFile("uid://1jaqfvkfofdo");
+	}
+
+	private void BackToMainMenuButtonPressed()
+	{
+		GameStateManager.Instance.ChangeState(GameState.MainMenu);
+		GetTree().ChangeSceneToFile("uid://booe2en3u4wmp"); // TODO it`s right exacly uid?
+	}
+
+	private void QuitTheGameButtonPressed() => GetTree().Quit();
 }
