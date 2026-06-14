4f7f7033af4c99707ba09a887e5a3dc3404dcdaa
trrt54999 <moki22810sobakacom@gmail.com>
2026-06-14 21:48:51 +0200

    feat: add handle rest method

--- GamePhaseManager.cs
+++ GamePhaseManager.cs
@@ -20,11 +20,23 @@ public partial class GamePhaseManager : Node2D
 			case GamePhase.Wave:
 				break;
 			case GamePhase.Rest:
+				_ = HandleRestPhase();
 				break;
 			case GamePhase.BossFight:
 				break;
 		}
 
 		OnPhaseChanged?.Invoke(Phase);
+	}
+
+	private async Task HandleRestPhase()
+	{
+		if (Phase != GamePhase.Rest)
+			return;
+
+		await ToSignal(GetTree().CreateTimer(5.0f), SceneTreeTimer.SignalName.Timeout);
+
+		int bossThreshold = 2500 * (GameSessionManager.Instance.DefeatedBosses + 1);
+		ChangePhase(GameSessionManager.Instance.Score >= bossThreshold ? GamePhase.BossFight : GamePhase.Wave);
 	}
 }
