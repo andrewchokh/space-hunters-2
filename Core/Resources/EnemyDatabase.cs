09ab345300123913f583f63b4689cadd45071f42
andrewchokh <andrew.chokh@gmail.com>
2026-09-14 15:29:56 +0300

    feat: add EnemyDatabase to Core/

---
+++ Core/Resources/EnemyDatabase.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class EnemyDatabase : Resource
+{
+	[Export]
+	public Array<EnemySpaceshipData> EnemyDataList { get; private set; }
+
+	public EnemySpaceshipData GetEnemyDataByID(string ID) {
+		var data = EnemyDataList.FirstOrDefault(e => e.ID == ID);
+
+		if (data != null) 
+			GD.PushWarning($"No enemy spaceship found by ID: {ID}");
+
+		return data;
+	} 
+
+	public Array<EnemySpaceshipData> GetEnemiesDataByTier(int tier) {
+		var dataArr = EnemyDataList.Where(e => e.Tier == tier).ToArray();
+
+		if (dataArr.Length == 0)
+			GD.PushWarning($"No enemy spaceship found by tier: {tier}");
+
+		// Converting System.Linq.Array to Godot.Collections.Array
+		return new Array<EnemySpaceshipData>(dataArr);
+	}
+}
